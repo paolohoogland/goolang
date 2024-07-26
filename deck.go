419345df3ab37fccc153e8c970b2eb6e276b28c8
phoogland <paolo.hoogland@etu.unistra.fr>
2024-07-26 09:42:04 +0200

    Added testing to the project

--- deck.go
+++ deck.go
@@ -17,7 +17,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Diamonds", "Hearts", "Spades", "Clubs"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
